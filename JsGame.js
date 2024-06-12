b94b377641b6ff9b8544b847f3d8827e019ec977
Lucas Stallbaum <lukasluca5489@gmail.com>
2024-06-12 20:36:07 -0300

    COMMIT 1
    Projeto sem backend

---
+++ JsGame.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#Start').click(function() {
+        $('.options').on('click', function() {
+            var OptionSelect = $(this).attr('id');
+            let Wins = false;
+            if(OptionSelect == 1){
+                anterior = 1
+            }
+
+        });
+    });
+});
